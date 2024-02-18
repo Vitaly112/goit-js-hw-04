c568060023368697a8692a019d0fedab6101c66c
Vitaly112 <vitalyishchenko@gmail.com>
2024-02-18 10:09:30 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,27 @@
-function makeTransaction (quantity, pricePerDroid) {
-    const sum = quantity * pricePerDroid;
-return `You ordered ${quantity} droids worth ${sum} credits!`;
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+function isEnoughCapacity(products, containerSize){
+    let sum = 0;
+    let valArr = Object.values(products);
+   for (let val of valArr) {
+     sum += val;
+   }
+    if(sum <= containerSize){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
